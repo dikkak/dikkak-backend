384d1c0587ea7dea50009bac86a398811b3e6cf4
jihyehann <haneh0714@gmail.com>
2022-04-14 13:45:56 +0900

    Initial commit

---
+++ src/main/java/com/dikkak/DikkakApplication.java
@@ -0,0 +1,13 @@
+package com.dikkak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DikkakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DikkakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dikkak/DikkakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dikkak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DikkakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
