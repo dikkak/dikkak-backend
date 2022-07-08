cb6f6bc22700eb38bfe3df36d010d38971a5575b
Han Jihye <haneh0714@gmail.com>
2022-07-08 18:08:54 +0900

    chore: 로그아웃 응답 추가

--- src/main/java/com/dikkak/dto/auth/LogoutRes.java
+++ src/main/java/com/dikkak/dto/auth/LogoutRes.java
@@ -1,2 +1,12 @@
-package com.dikkak.dto.auth;public class LogoutRes {
+package com.dikkak.dto.auth;
+
+import com.dikkak.entity.ProviderTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LogoutRes {
+
+    private ProviderTypeEnum provider;
 }
