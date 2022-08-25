0f0cdbe5504b09cee296c64a762039204b41a6b1
Han Jihye <haneh0714@gmail.com>
2022-08-25 21:59:26 +0900

    chore: allowedOrigin 추가

--- src/main/java/com/dikkak/config/WebMvcConfig.java
+++ src/main/java/com/dikkak/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://www.98o7.com")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
