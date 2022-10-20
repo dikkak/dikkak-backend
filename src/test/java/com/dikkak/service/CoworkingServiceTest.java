736659ab8c790d33f2637ee2e97ecfcca05894ce
Han Jihye <haneh0714@gmail.com>
2022-10-20 17:16:55 +0900

    FEAT: 테스트 코드 추가
    - 제안서 조회
    - 채팅 목록 조회

---
+++ src/test/java/com/dikkak/service/CoworkingServiceTest.java
@@ -0,0 +1,98 @@
+package com.dikkak.service;
+
+import com.dikkak.common.BaseException;
+import com.dikkak.dto.coworking.GetChattingRes;
+import com.dikkak.entity.coworking.*;
+import com.dikkak.entity.user.ProviderTypeEnum;
+import com.dikkak.entity.user.User;
+import com.dikkak.entity.user.UserTypeEnum;
+import com.dikkak.repository.UserRepository;
+import com.dikkak.repository.coworking.CoworkingFileRepository;
+import com.dikkak.repository.coworking.CoworkingMessageRepository;
+import com.dikkak.repository.coworking.CoworkingStepRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class CoworkingServiceTest {
+
+    @Autowired
+    CoworkingService coworkingService;
+
+    @Autowired
+    CoworkingMessageRepository messageRepository;
+
+    @Autowired
+    CoworkingFileRepository fileRepository;
+
+    @Autowired
+    CoworkingStepRepository stepRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("디자이너 매칭 & 외주작업실 생성")
+    @Transactional
+    void create() throws BaseException {
+        //given
+        User designer = userRepository.save(User.builder()
+                .email("user1@naver.com")
+                .providerType(ProviderTypeEnum.KAKAO)
+                .build());
+        designer.setUserType(UserTypeEnum.DESIGNER);
+
+        //when
+        Coworking savedCoworking = coworkingService.create(designer, 1L);
+
+        //then
+        assertThat(savedCoworking.getDesigner().getId()).isEqualTo(designer.getId());
+        assertThat(savedCoworking.getProgress()).isEqualTo(StepType.CHECK_PROPOSAL);
+    }
+
+    @Test
+    @DisplayName("채팅 목록 조회")
+    @Transactional
+    void getMessageList() throws BaseException, InterruptedException {
+        //given
+        User user1 = userRepository.save(User.builder()
+                .email("user1@naver.com")
+                .providerType(ProviderTypeEnum.KAKAO)
+                .build());
+        User user2 = userRepository.save(User.builder()
+                .email("user2@naver.com")
+                .providerType(ProviderTypeEnum.KAKAO)
+                .build());
+        CoworkingStep coworkingStep = stepRepository.findById(1L).get();
+        CoworkingMessage message1 = messageRepository.save(new CoworkingMessage(1L, user1, null, "텍스트 메시지1", coworkingStep));
+        CoworkingMessage message2 = messageRepository.save(new CoworkingMessage(2L, user2, null, "텍스트 메시지2", coworkingStep));
+
+
+        Thread.sleep(1000);
+        CoworkingFile file = fileRepository.save(CoworkingFile.builder().fileUrl("url1").fileName("파일 이름").coworkingStep(coworkingStep).build());
+        CoworkingMessage message3 = messageRepository.save(new CoworkingMessage(3L, user1, file, null, coworkingStep));
+
+        //when
+        List<GetChattingRes> messageList = coworkingService.getMessageList(1L, StepType.CHECK_PROPOSAL);
+
+        //then
+        for (int i=0; i<messageList.size(); i++)
+            System.out.println(i + " = " + messageList.get(i));
+
+        assertThat(messageList.size()).isEqualTo(3);
+        assertThat(messageList.get(0).getEmail()).isEqualTo(user1.getEmail());
+        assertThat(messageList.get(0).getContent()).isEqualTo(message1.getContent());
+        assertThat(messageList.get(1).getEmail()).isEqualTo(user2.getEmail());
+        assertThat(messageList.get(1).getContent()).isEqualTo(message2.getContent());
+        assertThat(messageList.get(2).getEmail()).isEqualTo(user1.getEmail());
+        assertThat(messageList.get(2).getFileUrl()).isEqualTo(message3.getCoworkingFile().getFileUrl());
+        assertThat(messageList.get(2).getFileName()).isEqualTo(message3.getCoworkingFile().getFileName());
+    }
+}
