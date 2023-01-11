19f8f14e772066cebdd5dfc6d1d7e6c1e98918ce
Han Jihye <haneh0714@gmail.com>
2023-01-11 10:17:47 +0900

    FEAT: LoginUser 어노테이션 추가

--- src/main/java/com/dikkak/config/WebMvcConfig.java
+++ src/main/java/com/dikkak/config/WebMvcConfig.java
@@ -1,13 +1,20 @@
 package com.dikkak.config;
 
+import com.dikkak.controller.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Value("${dikkak.origins}")
     private String[] origins;
@@ -20,5 +27,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
     }
 }
---
+++ src/main/java/com/dikkak/controller/LoginUserArgumentResolver.java
@@ -0,0 +1,52 @@
+package com.dikkak.controller;
+
+import com.dikkak.common.BaseException;
+import com.dikkak.config.UserPrincipal;
+import com.dikkak.entity.user.User;
+import com.dikkak.repository.UserRepository;
+import com.dikkak.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static com.dikkak.common.ResponseMessage.INVALID_ACCESS_TOKEN;
+
+@Component
+@RequiredArgsConstructor
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    static final String BEARER = "Bearer";
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(LoginUser.class) != null
+                && parameter.getParameterType().equals(UserPrincipal.class);
+    }
+
+    @Override
+    public UserPrincipal resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String authorization = webRequest.getHeader(HttpHeaders.AUTHORIZATION);
+        String token = extractToken(authorization);
+
+        // 토큰 검사 및 회원 아이디 추출
+        Long userId = jwtService.validateToken(token);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BaseException(INVALID_ACCESS_TOKEN));
+
+        return new UserPrincipal(user);
+    }
+
+    private static String extractToken(String authorization) {
+        if (authorization == null || !authorization.startsWith(BEARER + " ")) {
+            throw new BaseException(INVALID_ACCESS_TOKEN);
+        }
+        return authorization.substring(BEARER.length());
+    }
+}
