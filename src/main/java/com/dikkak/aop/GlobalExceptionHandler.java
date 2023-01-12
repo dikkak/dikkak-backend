08e4ee566bad0d2d452779493e3d888ad7055df6
Han Jihye <haneh0714@gmail.com>
2023-01-13 00:20:35 +0900

    FEAT: MethodArgumentNotValidException 핸들러 추가

--- src/main/java/com/dikkak/aop/GlobalExceptionHandler.java
+++ src/main/java/com/dikkak/aop/GlobalExceptionHandler.java
@@ -4,8 +4,11 @@ import com.dikkak.common.BaseException;
 import com.dikkak.common.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 import static com.dikkak.common.ResponseMessage.INVALID_ACCESS_TOKEN;
 
@@ -15,10 +18,22 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
         if(e.getResponseMessage().equals(INVALID_ACCESS_TOKEN)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e));
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ErrorResponse(e));
         } else {
-            return ResponseEntity.badRequest().body(new ErrorResponse(e));
+            return ResponseEntity.badRequest()
+                    .body(new ErrorResponse(e));
         }
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + "는 " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(message));
     }
 
     @ExceptionHandler
